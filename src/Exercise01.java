191d58bb94066811ee68731bd44b1d95b0ede911
ericweidman <ericweidman@gmail.com>
2016-02-11 10:22:33 -0500

    Add Exercise 01

---
+++ src/Exercise01.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by ericweidman on 2/11/16.
+ */
+public class Exercise01 {
+    public static void main(String[] args) {
+        String[] names = {"Alice,", "Bob", "Charlie", "David", "Edward", "Albert", "Beth"};
+        HashMap<Character, ArrayList<String>> nameMap = new HashMap();
+
+        //create empty arrayLists
+        for (String name : names){
+            char firstLetter = name.charAt(0);
+            nameMap.put(firstLetter, new ArrayList<String>());
+        }
+
+        //add names
+        for (String name : names){
+            char firstLetter = name.charAt(0);
+            nameMap.get(firstLetter).add(name);
+        }
+        System.out.println(nameMap);
+    }
+}
